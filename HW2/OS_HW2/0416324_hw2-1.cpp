58c91468e9d4db5891787fb67ed96a4bba196b20
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2017-10-25 23:22:25 +0800

    Prototype of OSHW2, problem1 done

---
+++ HW2/OS_HW2/0416324_hw2-1.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iomanip>
+#define pb push_back
+using namespace std;
+typedef vector<int> vi;
+string trick(int process_num)
+{
+    return "P["+to_string(process_num)+"]";
+}
+int main()
+{
+    fstream fptr;
+    fptr.open("Q1.txt");
+    int some,tcase,wt=0,tt=0,total_wt=0,total_tt=0;
+    vi process;
+    bool get_tcase=0;
+    while(fptr)
+    {
+        fptr>>some;
+        if(!get_tcase)
+        {
+            tcase=some;
+            get_tcase=1;
+        }
+        else
+        {
+            process.pb(some);
+        }
+    }
+    process.pop_back();
+    cout<<"Process     Waiting Time     Turnaround Time"<<endl;
+    for(int i=0;i<process.size();i++)
+    {
+        wt=(i==0)?0:(tt);
+        tt+=process[i];
+        total_tt+=tt;
+        total_wt+=wt;
+        cout<<setw(12)<<left<<trick(i+1)<<setw(17)<<left<<wt<<setw(14)<<left<<tt<<endl;
+    }
+    printf("Average waiting time: %lf\n",total_wt/(double)process.size());
+    printf("Average turnaround time: %lf\n",total_tt/(double)process.size());
+    return 0;
+}

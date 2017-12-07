f5119212235e172e1669f485d2f7beb9496f4f5f
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2017-12-07 19:39:45 +0800

    Added hw3-1 accelerated a little bit

--- HW3/0416324_hw3-1.cpp
+++ HW3/0416324_hw3-1.cpp
@@ -35,61 +35,61 @@ const char *outputBlur_name[5] =
 };
 
 unsigned char *pic_in, *pic_grey, *pic_blur, *pic_final;
-inline unsigned char RGB2grey(int w, int h)  ;
 inline unsigned char RGB2grey(int w, int h)
 {
-	int tmp =(pic_in[3 * (h*imgWidth + w) + MYRED] +pic_in[3 * (h*imgWidth + w) + MYGREEN] +pic_in[3 * (h*imgWidth + w) + MYBLUE])/3;
+	int tmp2=3 * (h*imgWidth + w) ;
+	int tmp =(pic_in[tmp2+MYRED]+pic_in[tmp2+MYGREEN]+pic_in[tmp2+MYBLUE])/3;
 	if (tmp < 0) tmp = 0;
 	if (tmp > 255) tmp = 255;
 	return (unsigned char)tmp;
 }
-inline unsigned char GaussianFilter(int w, int h)  ;
 inline unsigned char GaussianFilter(int w, int h)
 {
-	int tmp = 0;
+	int tmp=0,tmp2,tmp3,tmp4;
 	int a, b;
 	int ws = (int)sqrt((float)FILTER_SIZE);
-	for (int j = 0; j<ws; j++)
-	for (int i = 0; i<ws; i++)
+	for (register int j = 0; j<ws; j++)
 	{
-		a = w + i - (ws / 2);
+		tmp2=j*ws;
 		b = h + j - (ws / 2);
-
-		// detect for borders of the image
-		if (a<0 || b<0 || a>=imgWidth || b>=imgHeight) continue;
-
-		tmp += filter_G[j*ws + i] * pic_grey[b*imgWidth + a];
-	};
+		tmp3=b*imgWidth;
+		for (register int i = 0; i<ws; i++)
+		{
+			a = w + i - (ws / 2);
+			// detect for borders of the image
+			if(a<0 || b<0 || a>=imgWidth || b>=imgHeight) continue;
+			//inborder, do filter
+			tmp += filter_G[tmp2 + i] * pic_grey[tmp3 + a];
+		}
+	}
 	tmp /= FILTER_SCALE;
 	if (tmp < 0) tmp = 0;
 	if (tmp > 255) tmp = 255;
 	return (unsigned char)tmp;
 }
 //multithread image processing
-inline void* onethread_process_grey(void* args)  ;
 inline void* onethread_process_grey(void* args)
 {
 	long cur_thread=(long)args;
-
 	int upper_bound=cur_thread+1;
-	//last one, uneven divide
+	int tmp;
 	if(cur_thread==THREAD_CNT-1)
 	{
-		//printf("cur thread last %ld\n",cur_thread);
-		for(int i=cur_thread*onethread_height;i<imgHeight;i++)
+		for(register int i=cur_thread*onethread_height;i<imgHeight;i++)
 		{
-			for(int j=0;j<imgWidth;j++)
+			tmp=i*imgWidth;
+			for(register int j=0;j<imgWidth;j++)
 			{
-				pic_grey[i*imgWidth + j] = RGB2grey(j, i);
+				pic_grey[tmp + j] = RGB2grey(j, i);
 			}
 		}
 	}
 	else
 	{
-			//printf("cur thread %ld\n",cur_thread);
-		for(int i=cur_thread*onethread_height;i<onethread_height*upper_bound;i++)
+		for(register int i=cur_thread*onethread_height;i<onethread_height*upper_bound;i++)
 		{
-			for(int j=0;j<imgWidth;j++)
+			tmp=i*imgWidth;
+			for(register int j=0;j<imgWidth;j++)
 			{
 				pic_grey[i*imgWidth + j] = RGB2grey(j, i);
 			}
@@ -97,84 +97,61 @@ inline void* onethread_process_grey(void* args)
 	}
 	pthread_exit(EXIT_SUCCESS);
 }
-inline void multithread_grey()  ;
 inline void multithread_grey()
 {
-	int /*onethread_width = imgWidth / THREAD_CNT, */onethread_height = imgHeight / THREAD_CNT; //split by row
+	int onethread_height = imgHeight / THREAD_CNT; //split by row
 	pthread_t thread_id[THREAD_CNT];
-	/* pthread_create usage
-	int pthread_create(pthread_t *thread,
-				  const pthread_attr_t *restrict_attr,
-				  void*(*start_rtn)(void*),
-				  void *restrict arg);
-	*/
 	for(long cur_thread=0;cur_thread<THREAD_CNT;cur_thread++)
 	{
 		pthread_create(&thread_id[cur_thread],NULL,onethread_process_grey,(void *) cur_thread);
-		//printf("Grey cur thread id created %u\n",&thread_id[cur_thread]);
 	}
 	for(long  cur_thread=0;cur_thread<THREAD_CNT;cur_thread++)
 	{
-		//printf("Grey cur thread id pass in %u\n",&thread_id[cur_thread]);
 		pthread_join(thread_id[cur_thread],NULL);
 	}
-
 }
-inline void* onethread_process_gaussian(void* args)  ;
 inline void* onethread_process_gaussian(void* args)
 {
 	long cur_thread=(long)args;
-
 	int upper_bound=cur_thread+1;
-	//last one, uneven divide
+	register int tmp;
 	if(cur_thread==THREAD_CNT-1)
 	{
-		//printf("cur thread last %ld\n",cur_thread);
-		for(int i=cur_thread*onethread_height;i<imgHeight;i++)
+		for(register int i=cur_thread*onethread_height;i<imgHeight;i++)
 		{
-			for(int j=0;j<imgWidth;j++)
+			tmp=i*imgWidth;
+			for(register int j=0;j<imgWidth;j++)
 			{
-				pic_blur[i*imgWidth + j]=GaussianFilter(j/*col*/,i/*row*/);
+				pic_blur[tmp + j]=GaussianFilter(j/*col*/,i/*row*/);
 			}
 		}
 	}
 	else
 	{
-		//printf("cur thread %ld\n",cur_thread);
-		for(int i=cur_thread*onethread_height;i<onethread_height*upper_bound;i++)
+		for(register int i=cur_thread*onethread_height;i<onethread_height*upper_bound;i++)
 		{
-			for(int j=0;j<imgWidth;j++)
+			tmp=i*imgWidth;
+			for(register int j=0;j<imgWidth;j++)
 			{
-				pic_blur[i*imgWidth + j]=GaussianFilter(j/*col*/,i/*row*/);
+				pic_blur[tmp + j]=GaussianFilter(j/*col*/,i/*row*/);
 			}
 		}
 	}
 	pthread_exit(EXIT_SUCCESS);
 }
-inline void multithread_gaussian()  ;
 inline void multithread_gaussian()
 {
 	int /*onethread_width = imgWidth / THREAD_CNT, */onethread_height = imgHeight / THREAD_CNT; //split by row
 	pthread_t thread_id[THREAD_CNT];
-	/* pthread_create usage
-	int pthread_create(pthread_t *thread,
-				  const pthread_attr_t *restrict_attr,
-				  void*(*start_rtn)(void*),
-				  void *restrict arg);
-	*/
 	for(long  cur_thread=0;cur_thread<THREAD_CNT;cur_thread++)
 	{
 		pthread_create(&thread_id[cur_thread],NULL,onethread_process_gaussian,(void *) cur_thread);
-		//printf("Gaussian cur thread id created%u\n",&thread_id[cur_thread]);
 	}
 	for(long  cur_thread=0;cur_thread<THREAD_CNT;cur_thread++)
 	{
-		//printf("Gaussian cur thread id pass in%u \n",&thread_id[cur_thread]);
 		pthread_join(thread_id[cur_thread],NULL);
 	}
-
 }
-int main()  ;
 int main()
 {
 	// read mask file
@@ -188,31 +165,31 @@ int main()
 		fscanf(mask, "%d", &filter_G[i]);
 	fclose(mask);
 
-
+ 	register int tmp3,tmp4;
 	BmpReader* bmpReader = new BmpReader();
 	for (int k = 0; k<5; k++)
 	{
-		// read input BMP file
-		////printf("pic %d \n",k+1);
 		pic_in = bmpReader->ReadBMP(inputfile_name[k], &imgWidth, &imgHeight);
-		// allocate space for output image
-		pic_grey = (unsigned char*)malloc(imgWidth*imgHeight*sizeof(unsigned char));
-		pic_blur = (unsigned char*)malloc(imgWidth*imgHeight*sizeof(unsigned char));
-		pic_final = (unsigned char*)malloc(3 * imgWidth*imgHeight*sizeof(unsigned char));
+		tmp3=imgWidth*imgHeight*sizeof(unsigned char);
+		pic_grey = (unsigned char*)malloc(tmp3);
+		pic_blur = (unsigned char*)malloc(tmp3);
+		pic_final = (unsigned char*)malloc(tmp3+tmp3+tmp3);
+
 		pthread_mutex_lock(&mutex1);
 		multithread_grey();
 		multithread_gaussian();
 		pthread_mutex_unlock(&mutex1);
-		for (int j = 0; j<imgHeight; j++)
+		for (register int j = 0; j<imgHeight; j++)
 		{
-			for (int i = 0; i<imgWidth; i++)
+			tmp3=j*imgWidth;
+			for (register int i = 0; i<imgWidth; i++)
 			{
-				pic_final[3 * (j*imgWidth + i) + MYRED] = pic_blur[j*imgWidth + i];
-				pic_final[3 * (j*imgWidth + i) + MYGREEN] = pic_blur[j*imgWidth + i];
-				pic_final[3 * (j*imgWidth + i) + MYBLUE] = pic_blur[j*imgWidth + i];
+				tmp4=3 * (tmp3 + i);
+				pic_final[tmp4 + MYRED] = pic_blur[tmp3 + i];
+				pic_final[tmp4 + MYGREEN] = pic_blur[tmp3 + i];
+				pic_final[tmp4 + MYBLUE] = pic_blur[tmp3 + i];
 			}
 		}
-
 		// write output BMP file
 		bmpReader->WriteBMP(outputBlur_name[k], imgWidth, imgHeight, pic_final);
 
@@ -222,6 +199,11 @@ int main()
 		free(pic_blur);
 		free(pic_final);
 	}
-
 	return 0;
 }
+/* pthread_create usage
+int pthread_create(pthread_t *thread,
+			  const pthread_attr_t *restrict_attr,
+			  void*(*start_rtn)(void*),
+			  void *restrict arg);
+*/
--- HW3/0416324_hw3-2.cpp
+++ HW3/0416324_hw3-2.cpp
@@ -15,8 +15,6 @@ int FILTER_SIZE;
 int FILTER_SCALE;
 int *filter_G;
 
-pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
-
 const char *inputfile_name[5] =
 {
 	"input1.bmp",
@@ -35,15 +33,16 @@ const char *outputBlur_name[5] =
 };
 
 unsigned char *pic_in, *pic_grey, *pic_blur, *pic_final;
-unsigned char RGB2grey(int w, int h)
+inline unsigned char RGB2grey(int w, int h)
 {
-	int tmp =(pic_in[3 * (h*imgWidth + w) + MYRED] +pic_in[3 * (h*imgWidth + w) + MYGREEN] +pic_in[3 * (h*imgWidth + w) + MYBLUE])/3;
+	int tmp2=3*(h*imgWidth+w) ;
+	int tmp =(pic_in[tmp2+MYRED]+pic_in[tmp2+MYGREEN]+pic_in[tmp2+MYBLUE])/3;
 	if (tmp < 0) tmp = 0;
 	if (tmp > 255) tmp = 255;
 	return (unsigned char)tmp;
 }
 
-unsigned char sobel_filter(int w, int h)
+inline unsigned char sobel_filter(int w, int h)
 {
 	if (tmp < 0) tmp = 0;
 	if (tmp > 255) tmp = 255;
@@ -51,20 +50,21 @@ unsigned char sobel_filter(int w, int h)
 }
 //multithread image processing
 
-void* onethread_process_grey(void* args)
+inline void* onethread_process_grey(void* args)
 {
 	long cur_thread=(long)args;
 
 	int upper_bound=cur_thread+1;
-	//last one, uneven divide
+	register int tmp;
 	if(cur_thread==THREAD_CNT-1)
 	{
 		//printf("cur thread last %ld\n",cur_thread);
 		for(int i=cur_thread*onethread_height;i<imgHeight;i++)
 		{
+			tmp=i*imgWidth;
 			for(int j=0;j<imgWidth;j++)
 			{
-				pic_grey[i*imgWidth + j] = RGB2grey(j, i);
+				pic_grey[tmp + j] = RGB2grey(j, i);
 			}
 		}
 	}
@@ -73,31 +73,30 @@ void* onethread_process_grey(void* args)
 			//printf("cur thread %ld\n",cur_thread);
 		for(int i=cur_thread*onethread_height;i<onethread_height*upper_bound;i++)
 		{
+			tmp=i*imgWidth;
 			for(int j=0;j<imgWidth;j++)
 			{
-				pic_grey[i*imgWidth + j] = RGB2grey(j, i);
+				pic_grey[tmp=i*imgWidth; + j] = RGB2grey(j, i);
 			}
 		}
 	}
 	pthread_exit(EXIT_SUCCESS);
 }
-void multithread_grey()
+inline void multithread_grey()
 {
 	int onethread_height = imgHeight / THREAD_CNT; //split by row
 	pthread_t thread_id[THREAD_CNT];
 	for(long cur_thread=0;cur_thread<THREAD_CNT;cur_thread++)
 	{
 		pthread_create(&thread_id[cur_thread],NULL,onethread_process_grey,(void *) cur_thread);
-		//printf("Grey cur thread id created %u\n",&thread_id[cur_thread]);
 	}
 	for(long  cur_thread=0;cur_thread<THREAD_CNT;cur_thread++)
 	{
-		//printf("Grey cur thread id pass in %u\n",&thread_id[cur_thread]);
 		pthread_join(thread_id[cur_thread],NULL);
 	}
 
 }
-void* onethread_process_sobel(void* args)
+inline void* onethread_process_sobel(void* args)
 {
 	long cur_thread=(long)args;
 	int upper_bound=cur_thread+1;
@@ -127,7 +126,7 @@ void* onethread_process_sobel(void* args)
 	}
 	pthread_exit(EXIT_SUCCESS);
 }
-void multithread_gaussian()
+inline void multithread_sobel()
 {
 	int onethread_height = imgHeight / THREAD_CNT; //split by row
 	pthread_t thread_id[THREAD_CNT];
@@ -135,11 +134,9 @@ void multithread_gaussian()
 	for(long  cur_thread=0;cur_thread<THREAD_CNT;cur_thread++)
 	{
 		pthread_create(&thread_id[cur_thread],NULL,onethread_process_gaussian,(void *) cur_thread);
-		//printf("Gaussian cur thread id created%u\n",&thread_id[cur_thread]);
 	}
 	for(long  cur_thread=0;cur_thread<THREAD_CNT;cur_thread++)
 	{
-		//printf("Gaussian cur thread id pass in%u \n",&thread_id[cur_thread]);
 		pthread_join(thread_id[cur_thread],NULL);
 	}
 
@@ -156,8 +153,7 @@ int main()
 	for (int i = 0; i<FILTER_SIZE; i++)
 		fscanf(mask, "%d", &filter_G[i]);
 	fclose(mask);
-
-
+	register int tmp3,tmp4;
 	BmpReader* bmpReader = new BmpReader();
 	for (register int k = 0; k<5; k++)
 	{
@@ -166,18 +162,20 @@ int main()
 		// allocate space for output image
 		pic_grey = (unsigned char*)malloc(imgWidth*imgHeight*sizeof(unsigned char));
 		pic_blur = (unsigned char*)malloc(imgWidth*imgHeight*sizeof(unsigned char));
-		pic_final = (unsigned char*)malloc(3 * imgWidth*imgHeight*sizeof(unsigned char));
+		pic_final = (unsigned char*)malloc(3*imgWidth*imgHeight*sizeof(unsigned char));
 		pthread_mutex_lock(&mutex1);
 		multithread_grey();
-		multithread_gaussian();
+		multithread_sobel();
 		pthread_mutex_unlock(&mutex1);
 		for (register int j = 0; j<imgHeight; j++)
 		{
+			tmp3=j*imgWidth;
 			for (register int i = 0; i<imgWidth; i++)
 			{
-				pic_final[3 * (j*imgWidth + i) + MYRED] = pic_blur[j*imgWidth + i];
-				pic_final[3 * (j*imgWidth + i) + MYGREEN] = pic_blur[j*imgWidth + i];
-				pic_final[3 * (j*imgWidth + i) + MYBLUE] = pic_blur[j*imgWidth + i];
+				tmp4=3 * (tmp3 + i);
+				pic_final[tmp4 + MYRED] = pic_blur[tmp3 + i];
+				pic_final[tmp4 + MYGREEN] = pic_blur[tmp3 + i];
+				pic_final[tmp4 + MYBLUE] = pic_blur[tmp3 + i];
 			}
 		}
 

01ddbb843be61665f091fde4c95996990b35fe04
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2017-12-08 11:36:32 +0800

    Added hw3-1 using mutex more precisely

--- HW3/0416324_hw3-2.cpp
+++ HW3/0416324_hw3-2.cpp
@@ -5,7 +5,7 @@
 #include <math.h>
 #include <pthread.h>
 #include <semaphore.h>
-#define THREAD_CNT 6
+#define THREAD_CNT 4
 #define MYRED	2
 #define MYGREEN 1
 #define MYBLUE	0
@@ -15,7 +15,7 @@ int FILTER_SIZE;
 int FILTER_SCALE;
 int *filter_GX;
 int *filter_GY;
-
+sem_t binary_semaphore;
 const char *inputfile_name[5] =
 {
 	"input1.bmp",
@@ -36,7 +36,7 @@ const char *outputBlur_name[5] =
 unsigned char *pic_in, *pic_grey, *pic_blur, *pic_final;
 inline unsigned char RGB2grey(int w, int h)
 {
-	int tmp2=3*(h*imgWidth+w) ;
+int tmp2=3*(h*imgWidth+w) ;
 	int tmp =(pic_in[tmp2+MYRED]+pic_in[tmp2+MYGREEN]+pic_in[tmp2+MYBLUE])/3;
 	if (tmp < 0) tmp = 0;
 	if (tmp > 255) tmp = 255;
@@ -45,8 +45,9 @@ inline unsigned char RGB2grey(int w, int h)
 
 inline unsigned char sobel_filter(int w, int h)
 {
-	register int tmp=0,tmp2,tmp3;
-	register int a, b;
+	printf("w%d h%d \n",w,h);
+	register int tmp=0,tmp2=0,tmp3=0;
+	register int a=0, b=0;
 	int img_x=0,img_y=0;
 	int filter_border = (int)sqrt((float)FILTER_SIZE);
 	for (register int j = 0; j<filter_border; j++)
@@ -67,9 +68,7 @@ inline unsigned char sobel_filter(int w, int h)
 
 	if (img_x<0) img_x=0;
 	if (img_y<0) img_y=0;
-	if (tmp < 0) tmp = 0;
-	if (tmp > 255) tmp = 255;
-	return (unsigned char)tmp;
+	return ((int)(sqrt(img_x*img_x+img_y*img_y)))>255?255:(int)(sqrt(img_x*img_x+img_y*img_y));
 }
 //multithread image processing
 
@@ -113,16 +112,21 @@ inline void multithread_grey()
 	}
 	for(long  cur_thread=0;cur_thread<THREAD_CNT;cur_thread++)
 	{
+		//sem_trywait(&binary_semaphore); //try lock if not locked
 		pthread_join(thread_id[cur_thread],NULL);
+		cout<<777<<endl;
+		//sem_post(&binary_semaphore);
 	}
-
+	cout<<66<<endl;
 }
 inline void* onethread_process_sobel(void* args)
 {
+
 	long cur_thread=(long)args;
 	int upper_bound=cur_thread+1;
 	//last one, uneven divide
 	register int tmp;
+	printf("cur_thread %d\n",cur_thread);
 	if(cur_thread==THREAD_CNT-1)
 	{
 		for(int i=cur_thread*onethread_height;i<imgHeight;i++)
@@ -154,11 +158,16 @@ inline void multithread_sobel()
 
 	for(long  cur_thread=0;cur_thread<THREAD_CNT;cur_thread++)
 	{
+		cout<<888<<endl;
 		pthread_create(&thread_id[cur_thread],NULL,onethread_process_sobel,(void *) cur_thread);
+
 	}
 	for(long  cur_thread=0;cur_thread<THREAD_CNT;cur_thread++)
 	{
+		//sem_trywait(&binary_semaphore); //try lock if not locked
 		pthread_join(thread_id[cur_thread],NULL);
+		cout<<999<<endl;
+		//sem_post(&binary_semaphore);
 	}
 
 }
@@ -189,10 +198,14 @@ int main()
 		pic_grey = (unsigned char*)calloc(tmp3,sizeof(unsigned char));
 		pic_blur = (unsigned char*)calloc(tmp3,sizeof(unsigned char));
 		pic_final = (unsigned char*)calloc((tmp3<<1)+tmp3,sizeof(unsigned char));
-		pthread_mutex_lock(&mutex1);
+		//sem_init(&binary_semaphore,0/*only one process in this hw*/,6);
+		//open lock binary semaphore to let one process access the critical section at atime
+		printf("doing fray");
+		getchar();
 		multithread_grey();
+		printf("doing sobel ");
 		multithread_sobel();
-		pthread_mutex_unlock(&mutex1);
+
 		for (register int j = 0; j<imgHeight; j++)
 		{
 			tmp3=j*imgWidth;
--- HW3/test_all.sh
+++ HW3/test_all.sh
@@ -1,20 +1,6 @@
  #!/bin/bash
-g++ -std=c++11 -pthread 0416324_hw3-1.cpp
-#g++ -std=c++11 -pthread example_hw3-1.cpp
-
-./a.out
-#gprof ./a.out gmon.out -p
-./MAE.out ANS/Blur1.bmp Blur1.bmp
-./MAE.out ANS/Blur2.bmp Blur2.bmp
-./MAE.out ANS/Blur3.bmp Blur3.bmp
-./MAE.out ANS/Blur4.bmp Blur4.bmp
-./MAE.out ANS/Blur5.bmp Blur5.bmp
-
-./Speed.sh
-
-
 g++ -std=c++11 -pthread 0416324_hw3-2.cpp
-
+#g++ -std=c++11 -pthread example_hw3-1.cpp
 ./a.out
 #gprof ./a.out gmon.out -p
 ./MAE.out ANS/Sobel1.bmp Sobel1.bmp

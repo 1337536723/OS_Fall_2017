de40d64bd784aac6c98d26c7e93d413c001085a7
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2017-12-31 22:42:53 +0800

    Prototype of HW4

--- HW3/test_all.sh
+++ HW3/test_all.sh
@@ -10,7 +10,20 @@ g++ -std=c++11 -pthread 0416324_hw3-1.cpp
 ./MAE.out ANS/Blur5.bmp Blur5.bmp
 
 ./Speed.sh
+echo "tracher"
 
+g++ -std=c++11 -pthread example_hw3-1.cpp
+./a.out
+#gprof ./a.out gmon.out -p
+./MAE.out ANS/Blur1.bmp Blur1.bmp
+./MAE.out ANS/Blur2.bmp Blur2.bmp
+./MAE.out ANS/Blur3.bmp Blur3.bmp
+./MAE.out ANS/Blur4.bmp Blur4.bmp
+./MAE.out ANS/Blur5.bmp Blur5.bmp
+
+./Speed.sh
+
+echo "problem2"
 g++ -std=c++11 -pthread 0416324_hw3-2.cpp
 
 ./a.out
--- HW4/0416324_hw4.cpp
+++ HW4/0416324_hw4.cpp
@@ -10,24 +10,49 @@ Assume the TLB, page table, and physical memory is empty at the beginning.
 • 2^8 frames
 • Physical memory of 65536 bytes (256 frames * 256-byte frame size)
 
+Struecture: 3vector
+
 **************************************************************************************/
-struct content
+int tcase,tlb_miss,page_fault;
+struct table_content
 {
-    bool v; //valid bit
-    int last_access_time;
-}
-void simulation(int tcase)
-{
-    // for(int i=0;i<)
-}
+    int last_access_time,page_number,frame_number;
+};
+
 int main(int argc, char const *argv[])
 {
-    FILE* fptr;
-    fptr = fopen("address.txt","r")
+    //backing storage
+    FILE* BACK_fptr;
+    BACK_fptr = fopen(argv[1],"rb"); //read the binary
+    //address
+    fstream fptr;
+    fptr.open(argv[2]);
+    //output result
+    ofstream ofptr;
+    ofptr.open("result.txt",std::ofstream::out);
+
+    bool first=0;
+    tlb_miss=0;
+    page_fault=0;
     while(fptr)
     {
-        cout<<"Fptr reads "<<fptr<<endl;
+        int tmp;
+        fptr>>tmp;
+        if(!first)
+        {
+            first=1;
+            tcase=tmp;
+        }
+        cout<<"Fptr reads "<<tmp<<endl;
+        //search the TLB first
+
+        //TLB miss find the page table and load in
+
+        //page fault, load from BACKING STORE.
     }
-    
+
+    fclose(BACK_fptr);
+    fptr.close();
+    ofptr.close();
     return 0;
 }

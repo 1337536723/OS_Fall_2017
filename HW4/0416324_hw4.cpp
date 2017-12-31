3af9dd2ae2df11248392a0f339b9be4f9008d8ff
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2017-12-31 21:53:53 +0800

    Prototype of HW4

---
+++ HW4/0416324_hw4.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+/**************************************************************************************
+Specifics
+Assume the TLB, page table, and physical memory is empty at the beginning.
+• 2^4 entries in the TLB (Use LRU replacement Algorithm)
+• 2^8 entries in the page table
+• Page size of 2^8 bytes
+• Frame size of 2^8 bytes
+• 2^8 frames
+• Physical memory of 65536 bytes (256 frames * 256-byte frame size)
+
+**************************************************************************************/
+struct content
+{
+    bool v; //valid bit
+    int last_access_time;
+}
+void simulation(int tcase)
+{
+    // for(int i=0;i<)
+}
+int main(int argc, char const *argv[])
+{
+    FILE* fptr;
+    fptr = fopen("address.txt","r")
+    while(fptr)
+    {
+        cout<<"Fptr reads "<<fptr<<endl;
+    }
+    
+    return 0;
+}

a66c8ac59a7574d18dd2cc1046b0fc0b8b38f088
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-01-03 00:32:22 +0800

    DONE

--- HW4/0416324_hw4.cpp
+++ HW4/0416324_hw4.cpp
@@ -117,15 +117,22 @@ int main(int argc, char const *argv[])
                     accessed_value=phy_memory[page_table[page_num_addr]][offset_addr];
                 }
                 //Update the TLB by using LRU
-                int cnt=0;
+                /*int cnt=0;
                 for(int i=0;i<16;i++)
                 {
                     if(TLB[i].last_access_time!=-1)
                         cnt++;
                     cout<<TLB[i].page_number<<","<<TLB[i].frame_number<<","<<TLB[i].last_access_time<<"||";
                 }
-                cout<<endl;
+                cout<<"\n--------------------------------------------------------"<<endl;*/
                 sort(TLB.begin(), TLB.end(), LRU_compare);
+                /*for(int i=0;i<16;i++)
+                {
+                    if(TLB[i].last_access_time!=-1)
+                        cnt++;
+                    cout<<TLB[i].page_number<<","<<TLB[i].frame_number<<","<<TLB[i].last_access_time<<"||";
+                }
+                cout<<endl<<endl;*/
                 TLB[0].page_number=page_num_addr;
                 TLB[0].frame_number=page_table[page_num_addr];
                 TLB[0].last_access_time=timestamp;
